dbcb8a444d3daf3ffff2c03564af03311a6963df
karasu-huginn <erwann.quin@supinfo.com>
2025-01-29 15:53:46 +0100

    feat: working Connector

--- src/db_classes/Connector.java
+++ src/db_classes/Connector.java
@@ -1,6 +1,8 @@
 package db_classes;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Connector {
     Statement statement;
@@ -20,46 +22,76 @@ public class Connector {
         statement = connection.createStatement();
     }
 
-    public Boolean is_connected() {
-        // send DB simple read
-        // check if DB responds
-        return true;
-    }
-
-    public String read(String table) {
-        String sql_req = "SELECT * FROM " + table;
-
+    public List<String> read(String table, String column) {
+        String sql_req = "SELECT " + column + " FROM " + table;
+        String column_name;
+        String value;
+        List<String> outputs = new ArrayList<>();
         try {
-            statement.executeQuery(sql_req);
+            ResultSet result_set = statement.executeQuery(sql_req);
+            java.sql.ResultSetMetaData rsmd = result_set.getMetaData();
+            while (result_set.next()) {
+                for(int i = 1; i <= rsmd.getColumnCount(); i++) {
+                    column_name = rsmd.getColumnName(i);
+                    value = result_set.getString(column_name);
+                    outputs.add(value);
+                }
+            }
         } catch (SQLException e) {
             e.fillInStackTrace();
             throw new RuntimeException(e);
         }
-        return "";
+        return outputs;
     }
 
-    public String read(String table, String filter, String filter_value) {
-        String sql_req = "SELECT * FROM " + table + " WHERE " + filter + " = " + filter_value;
-
+    public String read(String table, String column, String filter, String filter_value) {
+        String sql_req = "SELECT " + column + " FROM " + table + " WHERE " + filter + " = " + filter_value;
+        String column_name;
+        String value = "QUERY_ERROR";
         try {
-            statement.executeQuery(sql_req);
+            ResultSet result_set = statement.executeQuery(sql_req);
+            java.sql.ResultSetMetaData rsmd = result_set.getMetaData();
+            while (result_set.next()) {
+                for(int i = 1; i <= rsmd.getColumnCount(); i++) {
+                    column_name = rsmd.getColumnName(i);
+                    value = result_set.getString(column_name);
+                }
+            }
         } catch (SQLException e) {
             e.fillInStackTrace();
             throw new RuntimeException(e);
         }
-        return "";
+        return value;
     }
 
-    public void create() {
-        // create sql_req
+    public void create(String table, String columns, String values) {
+        String sql_req = "INSERT INTO " + table + "(" + columns + ") VALUES (" + values + ")";
+        try {
+            statement.execute(sql_req);
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public void update() {
-        // create sql_req
+    public void update(String table, String column, String value, String id) {
+        String sql_req = "UPDATE "+ table +" SET " + column + " = '" + value + "' WHERE " + table + ".id = " + id;
+        try {
+            statement.execute(sql_req);
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public void delete() {
-        // create sql_req
+    public void delete(String table, String id) {
+        String sql_req = "DELETE FROM " + table + " WHERE " + table + ".id = " + id + ";";
+        try {
+            statement.execute(sql_req);
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
